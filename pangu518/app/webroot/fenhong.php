38dc7f76ea4e5024e87e7e4590f31884a9d4df7c
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-08-11 03:24:24 +0000

    网站

---
+++ pangu518/app/webroot/fenhong.php
@@ -0,0 +1,104 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="zh-cn">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>PG</title>
+<style type="text/css">
+<!--
+a:link {
+	color: #333333;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #333333;
+}
+a:hover {
+	text-decoration: none;
+	color: #333333;
+}
+a:active {
+	text-decoration: none;
+}
+.style1 {font-size: 16px}
+.STYLE3 {
+	color: #808080;
+	font-size: 12px;
+}
+-->
+</style>
+</head>
+
+<body topmargin="0" leftmargin="0">
+<?php	include("header.php");?>
+<table width="900" height="825" align="center" cellpadding="0" cellspacing="0">
+	<!-- MSTableType="layout" -->
+	<tr>
+		<td width="197" height="129" valign="top" background="img_www/fenhongT01.jpg">　</td>
+		<td width="612" height="129" valign="top" background="img_www/fenhongT02.jpg">
+		　</td>
+		<td width="91" height="129" valign="top" background="img_www/fenhongT03.jpg">　</td>
+	</tr>
+	<tr>
+		<td width="197" height="47" valign="top" background="img_www/fenhongT04.jpg">　</td>
+		<td width="612" height="47" valign="middle" background="img_www/botton1.jpg">
+	  <p align="center">
+		<font face="黑体" size="2"><a href="main.php">首页</a></font><font face="黑体" size="2" color="#666666">|<a href="qiye.php">企业文化</a><font color="#666666">|</font><a href="falv.php">法律法规</a><font color="#666666">|</font><a href="fenhong.php">分红规则</a><font color="#666666">|</font><a href="qiye.php">客户服务中心</a><font color="#666666">|</font><a href="huikuan.php">汇款内容</a><font color="#666666">|</font><a href="VIPuse.php">会员消费单位</a><font color="#666666">|</font><a href="xiazai.php">下载专区</a><font color="#666666">|</font><a href="vip.php">会员专区</a>|盘古论坛</font></td>
+		<td width="91" height="47" valign="top" background="img_www/fenhongT05.jpg">　</td>
+	</tr>
+	<tr>
+		<td width="197" height="73" valign="top" rowspan="2">
+		<img border="0" src="img_www/fenhonggueizhe.jpg" width="197" height="73"></td>
+		<td width="612" height="35" valign="top" background="img_www/background4.jpg">　</td>
+		<td width="91" valign="top" rowspan="5">　</td>
+	</tr>
+	<tr>
+		<td width="612" height="38" valign="top" background="img_www/fenhongB05.jpg">　</td>
+	</tr>
+	<tr>
+		<td width="197" height="36" valign="top">
+		<img border="0" src="img_www/side2.jpg" width="197" height="36"></td>
+		<td width="612" height="36" valign="top" background="img_www/fenhongB06.jpg">　</td>
+	</tr>
+	<tr>
+		<td width="197" height="224" valign="top">
+		<img height="33" src="img_www/side3.jpg" width="197" border="0"><img height="33" src="img_www/side4.jpg" width="197" border="0"><img height="33" src="img_www/side5.jpg" width="197" border="0"><img height="33" src="img_www/side6.jpg" width="197" border="0"><img height="51" src="img_www/side7.jpg" width="197" border="0"><img height="41" src="img_www/side8.jpg" width="197" border="0"></td>
+		<td width="612" valign="top" rowspan="2">　
+		  <table width="600" border="0" align="right" cellpadding="0" cellspacing="0" id="table1">
+			<tr>
+				<td align="left" valign="top"><p class="STYLE3">四川盘古经济咨询有限责任公司 <br>
+				  盘古消费资本财富分红规则 <br>
+				  四川盘古经济咨询有限责任公司是以世界新经济研究院院长，英、美、日、加拿大等多家跨国公司高级经济顾问陈瑜教授所著《消费者也能成为“资本家”》一书之消费资本化理论为基础，通过消费作为纽带，把生产者、经营者和消费者密切联系起来，最终以电子商务系统为运行平台的消费资本财富分红的营运模式，达到三方共赢、共同创造消费资本财富之目的。下面就将盘古消费资本财富分红规则约定如下： <br>
+				  一、四川盘古经济咨询有限责任公司开发并发行盘古消费资本财富分红资格凭证，每张面值为两元。<br>
+				  二、盘古消费资本财富分红规则，是以一个3位自然数为分红号码，个人会员和会员消费单位从000-999的数字中任意选择一个3位自然数录入本公司网站进行分红。每次编写的号码和当期开出的分红号码相同，即可享受当期分红，每期分红金额最高为4999元（含税）。（注：详细操作方法请参照＜个人会员和会员消费单位权、责、利＞）。 <br>
+				  三、个人会员和会员消费单位参与本公司分红的细则：<br>
+				  1、个人会员参与分红的，如消费过程中获得本公司盘古消费资本财富分红资格凭证1张（每张2元），即有一次分红资格；如消费过程中获得本公司盘古消费资本财富分红资格凭证50张（共计100元），即有50次分红资格，依次类推。 <br>
+				  2、以消费单位参与分红的，如一次在工作站购本公司盘古消费资本财富分红资格凭证25张（共计50元），即有一次分红资格；如一次在工作站购本公司盘古消费资本财富分红资格凭证50张（共计100元），即有2次分红资格，依次类推。 <br>
+				  3、盘古消费资本财富每期分红总金额是当期个人会员登录盘古消费财富网，以个人会员身份录入系统的盘古消费资本财富分红资格凭证总额的50%，如当期个人会员录入盘古消费资本财富分红资格凭证的总额为100万元，即当期个人会员和会员消费单位的分红总金额为50万元；如当期个人会员录入盘古消费资本财富分红资格凭证的总额为200万元，即当期个人会员和会员消费单位的分红总金额为100万元，依次类推。 <br>
+				  4、具体分红金额：如当期分红总金额为100万元，有150个盘古消费资本财富分红资格编写的三位数与当期开出的三位数相同，即每个盘古消费资本财富分红资格获分红金额4999元（含税），余下的转入下期分红；如有400个盘古消费资本财富分红资格编写的三位数与当期开出的三位数相同即每个盘古消费资本财富分红资格获分红金额2500元（含税），依次类推。 <br>
+				  四、分红号码在四川盘古经济咨询有限责任公司大厅上午10：00公开进行，在公证人员监督下，由本公司抽中的两个会员和一个会员消费单位的代表组成当期分红号码小组，以最传统的抓阄方式，三个代表每人依次抓一个数字，组成当期分红号码。并同时开通公司网上在线，当日即在公司网站上公布分红号码和分红金额，并于次日将分红金额（税后）转入所获分红号码的个人会员和会员消费单位代表的邮政储蓄卡上。 <br>
+				  五、四川盘古经济咨询有限责任公司负责抽出分红号码的工作，从第一期分红号码开出后，每两天在公司所在地抽出当期分红号码，全国共享统一的一个号码。 <br>
+  六、此规则如有修改公司会提前在网站上发出公告。 <br>
+				  七、此规则最终解释权归四川盘古经济咨询有限责任公司所有。 <br>
+				  <br> 
+					<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;四川盘古经济咨询有限责任公司</strong> <br>
+					<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;二00七年七月 </strong></p>				
+				</td>
+			</tr>
+		  </table>
+	  </td>
+	</tr>
+	<tr>
+		<td width="197" valign="top" background="img_www/background2.jpg">　</td>
+	</tr>
+	<tr>
+		<td width="197" height="20" valign="top" background="img_www/background1.jpg">　</td>
+		<td width="612" height="20" valign="top" background="img_www/background3.jpg">　</td>
+		<td width="91" height="20" valign="top" background="img_www/top8.jpg">　</td>
+	</tr>
+</table>
+<?php	include("footer.php");?>
+</body>
+
+</html>
