430511d506f43a51e04d30d8eb5228645a601dfb
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-08-15 05:06:23 +0000

    header不要广告

--- pangu518/app/webroot/header.php
+++ pangu518/app/webroot/header.php
@@ -6,7 +6,11 @@
       <embed src="img_www/logo.swf" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" name="obj1" width="60" height="60" quality="High">    
     </object></td>
     <td width="215" valign="top"><img border="0" src="img_www/logo3.jpg" width="215" height="62"></td>
-    <td align="right" valign="top"><img border="0" src="img_www/shouyeguanggao.JPG" width="424" height="62"></td>
+    <td align="right" valign="top">
+	<!--
+	<img border="0" src="img_www/shouyeguanggao.JPG" width="424" height="62">
+	-->
+	</td>
     <td width="2" align="right" valign="top">&nbsp;</td>
   </tr>
 </table>
