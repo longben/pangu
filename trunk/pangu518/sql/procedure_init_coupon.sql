287d0ec33c5d63f282dfe4dae586f0ad256297fc
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-08-21 18:53:42 +0000

    update sql

--- trunk/pangu518/sql/procedure_init_coupon.sql
+++ trunk/pangu518/sql/procedure_init_coupon.sql
@@ -4,6 +4,7 @@ begin
     declare _pwd  char(6);
     declare _no char(10);
     declare _count int;
+
     set _count = 0;
     insert into coupon_lists(coupon_start,coupon_end,coupon_group,custom_num)
       values(_start,_end,_group,_num);
@@ -19,4 +20,5 @@ begin
         set _start = _start + 1;
 	set _count = _count + 1;
     end while;
+	commit;
 end 
