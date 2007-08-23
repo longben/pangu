1939cd7b7bdf4c22aebcb91c15800a8a00cc664e
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-08-23 16:19:20 +0000

    update procedure

--- trunk/pangu518/sql/procedure_init_coupon.sql
+++ trunk/pangu518/sql/procedure_init_coupon.sql
@@ -1,4 +1,4 @@
-create or replace procedure init_coupon(out flag int,in _start int ,in _end int ,in _num int ,in _group char)
+create procedure init_coupon(out flag int,in _start int ,in _end int ,in _num int ,in _group char)
 begin 
     declare _random  char(6);
     declare _pwd  char(6);
@@ -6,26 +6,37 @@ begin
     declare _count int;
 
     set _count = 0;
-    insert into coupon_lists(coupon_start,coupon_end,coupon_group,custom_num,status)
+
+    -- insert list data
+	insert into coupon_lists(coupon_start,coupon_end,coupon_group,custom_num,status)
       values(_start,_end,_group,_num,0);
     commit;
-    while  _start <= _end do
-        set _random = rpad(rand(_start) * 1000000,6,'0');
-	set _pwd = rpad(_random ^ _num,6,'0');
-	set _no = concat(_group,lpad(_start,9,'0'));
-	insert into coupons(coupon_no,coupon_pwd,custom_num,random_num,coupon_group) 
-	    values(_no,_pwd,_num,_random,_group);
-	if mod(_count,10000) = 0 then 
-	  commit;
-	end if;
+
+	-- record list id
+	select id INTO @lid FROM coupon_lists 
+			  where coupon_start = _start and coupon_end = _end 
+					and coupon_group = _group and custom_num = _num and status = 0;
+
+    -- 生成代金券
+	while  _start <= _end do
+		-- 开始执行更改list状态
+		IF _count = 0 THEN 
+			update coupon_lists set status = 1 where id = @lid;
+		END IF ;
+		set _random = rpad(rand(_start) * 1000000,6,'0');
+		set _pwd = rpad(_random ^ _num,6,'0');
+		set _no = concat(_group,lpad(_start,9,'0'));
+		insert into coupons(coupon_no,coupon_pwd,custom_num,random_num,coupon_group) 
+			values(_no,_pwd,_num,_random,_group);
+		if mod(_count,10000) = 0 then 
+		  commit;
+		end if;
         set _start = _start + 1;
-	set _count = _count + 1;
+		set _count = _count + 1;
     end while;
-    update coupon_lists set status = 1 
-      where coupon_start = _start 
-        and coupon_end = _end 
-	and coupon_group = _group
-	and custom_num = _num
-	and status = 0;
+
+    -- 生成完毕，更新list状态
+	update coupon_lists set status = 2 where id = @lid;
+
     commit;
 end 
