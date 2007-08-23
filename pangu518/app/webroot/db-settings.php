ce70c303a09bb875d4f9008b49dacc69b27c8ba9
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-08-23 13:42:10 +0000

    www db link

--- pangu518/app/webroot/db-settings.php
+++ pangu518/app/webroot/db-settings.php
@@ -4,7 +4,7 @@
 	$db_name 		= "pangu518";
 	$db_host 		= "localhost";
 	$db_user 		= "root";
-	$db_password 	= "";	
+	$db_password 	= "www.pangu518.com";	
 
 	$db = mysql_connect($db_host, $db_user, $db_password);
 	mysql_query("set names 'utf8'");
