fb0f9a0194f130cb2ca0a2c05d0c39564fdf8dc6
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-09-01 16:35:18 +0000

    网站最终页面调整

--- trunk/pangu518/app/webroot/view.php
+++ trunk/pangu518/app/webroot/view.php
@@ -18,22 +18,72 @@
 ?>
 <html>
 <head>
+<meta http-equiv="Content-Language" content="zh-cn">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title><?=$arrNEWS[2]?></title>
 <link href="css/style.css" rel="stylesheet" type="text/css">
+<TITLE>盘古经济</TITLE>
 </head>
-<body>
-<p>&nbsp;</p>
-<table width="400" border="0" align="center" cellpadding="1" cellspacing="1" bgcolor="#000033">
-  <tr>
-    <td height="35" align="center" bgcolor="#FFFFFF"><span class="title"><?=$arrNEWS[0]?></span></td>
-  </tr>
-  <tr>    
-	<td height="150" valign="top" bgcolor="#FFFFFF" class="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?=$arrNEWS[1]?></td>
-  </tr>
-  <tr>
-    <td height="15" align="center" bgcolor="#FFFFFF">[<a href="javascript:window.close()">关闭窗口</a>]</td>
-  </tr>
+
+<body topmargin="0" leftmargin="0">
+<?php	include("header.php");?>
+<table width="900" align="center" cellpadding="0" cellspacing="0">
+	<tr>
+		<td width="197" height="73" valign="top" rowspan="2">
+		<img border="0" src="img_www/jingjizixun.jpg" width="197" height="73"></td>
+		<td width="612" height="35" valign="top" background="img_www/background4.jpg">&nbsp;</td>
+		<td width="91" valign="top" rowspan="5"><?php include("right_gg.php");?></td>
+	</tr>
+	<tr>
+		<td width="612" height="38" valign="top" background="img_www/qiyeB05.jpg">&nbsp;</td>
+	</tr>
+	<tr>
+		<td width="197" height="36" valign="top">&nbsp;</td>
+		<td width="612" height="36" valign="top" background="img_www/qiyeB06.jpg"></td>
+	</tr>
+	<tr>
+		<td width="197" height="224" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td height="36" valign="middle" background="img_www/sidebg2.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="contact.php">&nbsp;<span class="left_title">联系盘古</span></a></td>
+          </tr>
+          <tr>
+            <td height="36" valign="middle" background="img_www/sidebg2.jpg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="join.php">&nbsp;<span class="left_title">加盟盘古</span></a></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><a href="cooperation.php"><img src="img_www/side7.jpg" width="197" height="51" border="0"></a></td>
+          </tr>
+          <tr>
+            <td><img src="img_www/side8.jpg" width="197" height="41" border="0"></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+        </table></td>
+		<td width="612" valign="top" rowspan="2" align="center"><table width="90%" border="0" align="center" cellpadding="1" cellspacing="1" bgcolor="#E8E8E8">
+          <tr>
+            <td height="35" align="center" bgcolor="#FFFFFF"><span class="title">
+              <?=$arrNEWS[0]?>
+            </span></td>
+          </tr>
+          <tr>
+            <td height="300" valign="top" bgcolor="#FFFFFF" class="text">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <?=$arrNEWS[1]?></td>
+          </tr>
+
+        </table></td>
+	</tr>
+	<tr>
+		<td width="197" valign="top" background="img_www/background2.jpg">　</td>
+	</tr>
+	<tr>
+		<td width="197" height="20" valign="top" background="img_www/background1.jpg">　</td>
+		<td width="612" height="20" valign="top" background="img_www/background3.jpg">　</td>
+		<td width="91" height="20" valign="top" background="img_www/top8.jpg">　</td>
+	</tr>
 </table>
+<?php	include("footer.php");?>
 </body>
+
 </html>
