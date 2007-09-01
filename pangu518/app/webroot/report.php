371c0dea5c531497438c026926738775af3623f7
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-09-01 12:12:59 +0000

    update  当月统计

--- pangu518/app/webroot/report.php
+++ pangu518/app/webroot/report.php
@@ -1,5 +1,8 @@
 <?php
 	require_once('db-settings.php');
+
+	$reportYear = date("Y");
+	$reportMonth = date("m");
 ?>
 <html>
 <head>
@@ -13,7 +16,7 @@
 <!-- 选择45×25CM的纸张，横向打印  -->
 <table width="100%" border="1" align="center" cellpadding="0" cellspacing="0" bordercolor="#33FFCC">
   <tr>
-    <td colspan="2" rowspan="2" align="center" bgcolor="#CCFFCC" class="title"><?=date('Y')?>年</td>
+    <td colspan="2" rowspan="2" align="center" bgcolor="#CCFFCC" class="title"><?=$reportYear?>年</td>
     <td width="70" rowspan="2" align="center" bgcolor="#CCFFCC">新增会员总数</td>
     <td colspan="31" align="center" bgcolor="#CCFFCC">新增会员分省总数</td>
     <td width="140" rowspan="2" align="center" bgcolor="#CCFFCC">新增会员消费单位总数</td>
@@ -52,7 +55,7 @@
   
 <?php
 	//循环当月天数·start
-	for($i =1;$i<=date('t');$i++){
+	for($i =1;$i<=30;$i++){
 ?>
   <tr>
 
@@ -60,7 +63,7 @@
 		//显示第一列月份，且根据当月的天数判断rowspan
 		if($i == 1){
   ?>
-    <td rowspan="<?=date('t')?>" align="center" class="title"><?=date('n')?>月</td>
+    <td rowspan="<?=date('t')?>" align="center" class="title"><?=$reportMonth?>月</td>
   <?php	}?>
 
 	<!--显示第二列天-->
@@ -68,7 +71,7 @@
 
 	<!-- 显示会员总数数据 -->
 	<?php
-		$sql_member_total = " select count(*) from users where to_days(created) = (to_days(now())-".(date('t')-$i).")";	
+		$sql_member_total = " select count(*) from users where substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i";	
 		$stmt_member_total = mysql_query($sql_member_total);
 		while($arr_member_total = mysql_fetch_array($stmt_member_total)){
 			echo "<td align='center' class='title_small'>$arr_member_total[0]</td>";
@@ -81,7 +84,7 @@
 		$stmt_member_sheng = mysql_query($sql_member_sheng);
 		while($arr_member_sheng= mysql_fetch_array($stmt_member_sheng)){
 	
-			$sql_member_count = "select count(*) from users where region_id = ".$arr_member_sheng[0]." and to_days(created) = (to_days(now())-".(date('t')-$i).")";
+			$sql_member_count = "select count(*) from users where region_id = ".$arr_member_sheng[0]." and substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i";
 			$stmt_member_count = mysql_query($sql_member_count);
 			while ($arr_member_count = mysql_fetch_array($stmt_member_count)){
 				echo "<td align='center'>";
@@ -97,19 +100,19 @@
 
 	<!-- 显示会员消费单位总数、已审核总数、未审核总数  0无效  1有效  9待审核 -->
 	<?php
-		$sql_merchant_total = " select count(*) from merchants where to_days(created) = (to_days(now())-".(date('t')-$i).")";	
+		$sql_merchant_total = " select count(*) from merchants where substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i";	
 		$stmt_merchant_total = mysql_query($sql_merchant_total);
 		while($arr_merchant_total = mysql_fetch_array($stmt_merchant_total)){
 			echo "<td align='center' class='title_small'>$arr_merchant_total[0]</td>";
 		}
 
-		$sql_merchant_total_1 = " select count(*) from merchants where to_days(created) = (to_days(now())-".(date('t')-$i).") and status = 1";	
+		$sql_merchant_total_1 = " select count(*) from merchants where substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i and status = 1";	
 		$stmt_merchant_total_1 = mysql_query($sql_merchant_total_1);
 		while($arr_merchant_total_1 = mysql_fetch_array($stmt_merchant_total_1)){
 			echo "<td align='center' class='title_small'>$arr_merchant_total_1[0]</td>";
 		}
 
-		$sql_merchant_total_9 = " select count(*) from merchants where to_days(created) = (to_days(now())-".(date('t')-$i).") and status = 9";	
+		$sql_merchant_total_9 = " select count(*) from merchants where substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i and status = 9";	
 		$stmt_merchant_total_9 = mysql_query($sql_merchant_total_9);
 		while($arr_merchant_total_9 = mysql_fetch_array($stmt_merchant_total_9)){
 			echo "<td align='center' class='title_small'>$arr_merchant_total_9[0]</td>";
@@ -122,7 +125,7 @@
 		$stmt_merchant_sheng = mysql_query($sql_merchant_sheng);
 		while($arr_merchant_sheng= mysql_fetch_array($stmt_merchant_sheng)){
 	
-			$sql_merchant_count = "select count(*) from merchants where region_id = ".$arr_merchant_sheng[0]." and to_days(created) = (to_days(now())-".(date('t')-$i).")";
+			$sql_merchant_count = "select count(*) from merchants where region_id = ".$arr_merchant_sheng[0]." and  substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i";
 			$stmt_merchant_count = mysql_query($sql_merchant_count);
 			while ($arr_merchant_count = mysql_fetch_array($stmt_merchant_count)){
 				echo "<td align='center'>";
@@ -139,19 +142,19 @@
 
 	<!-- 显示工作站总数、已审核总数、未审核总数  0无效  1有效  9待审核 -->
 	<?php
-		$sql_workstation_total = " select count(*) from workstations where to_days(created) = (to_days(now())-".(date('t')-$i).")";	
+		$sql_workstation_total = " select count(*) from workstations where substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i";	
 		$stmt_workstation_total = mysql_query($sql_workstation_total);
 		while($arr_workstation_total = mysql_fetch_array($stmt_workstation_total)){
 			echo "<td align='center' class='title_small'>$arr_workstation_total[0]</td>";
 		}
 
-		$sql_workstation_total_1 = " select count(*) from workstations where to_days(created) = (to_days(now())-".(date('t')-$i).") and status = 1";	
+		$sql_workstation_total_1 = " select count(*) from workstations where substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i and status = 1";	
 		$stmt_workstation_total_1 = mysql_query($sql_workstation_total_1);
 		while($arr_workstation_total_1 = mysql_fetch_array($stmt_workstation_total_1)){
 			echo "<td align='center' class='title_small'>$arr_workstation_total_1[0]</td>";
 		}
 
-		$sql_workstation_total_9 = " select count(*) from workstations where to_days(created) = (to_days(now())-".(date('t')-$i).") and status = 9";	
+		$sql_workstation_total_9 = " select count(*) from workstations where substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i and status = 9";	
 		$stmt_workstation_total_9 = mysql_query($sql_workstation_total_9);
 		while($arr_workstation_total_9 = mysql_fetch_array($stmt_workstation_total_9)){
 			echo "<td align='center' class='title_small'>$arr_workstation_total_9[0]</td>";
@@ -165,7 +168,7 @@
 		$stmt_workstation_sheng = mysql_query($sql_workstation_sheng);
 		while($arr_workstation_sheng= mysql_fetch_array($stmt_workstation_sheng)){
 	
-			$sql_workstation_count = "select count(*) from workstations where region_id = ".$arr_workstation_sheng[0]." and to_days(created) = (to_days(now())-".(date('t')-$i).")";
+			$sql_workstation_count = "select count(*) from workstations where region_id = ".$arr_workstation_sheng[0]." and substring(created,1,4) = '$reportYear' and substring(created,6,2) = '$reportMonth' and substring(created,9,2) = $i";
 			$stmt_workstation_count = mysql_query($sql_workstation_count);
 			while ($arr_workstation_count = mysql_fetch_array($stmt_workstation_count)){
 				echo "<td align='center'>";
