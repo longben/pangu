e3ac2580aaec52b01e91d828cfa3e0921568d049
longben <longben@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-09-01 14:10:08 +0000

    解决分页问题

--- trunk/pangu518/app/controllers/regions_controller.php
+++ trunk/pangu518/app/controllers/regions_controller.php
@@ -3,40 +3,33 @@ class RegionsController extends AppController {
 
 	var $name = 'Regions';
 	var $helpers = array('Html', 'Form' , 'Ajax', 'Javascript','Pagination');
-	var $components = array ('Pagination','Pager'); // Added
+	var $components = array ('Pagination'); // Added
 
 
-	function index($page=1,$keyword='') {
+	function index($keyword= null, $page=1) {
         
-		$key = $this->data['Region']['keyword'];
-		if($key){
-			$criteria = array(
-				  'Region.id' => 'LIKE %'. $key .'%',
-				);
-		}else{
-			$criteria = null;
+		$criteria = null;
+		if($keyword == null){
+			$keyword = $this->data['Region']['keyword'];
 		}
+			
+		$this->Pagination->resultsPerPage = array(50,100,200,500);
+		$this->Pagination->show = 15;
+		//$this->Pagination->style = "ajax"; //ajax风格翻页
 
-        echo key($this->Pagination->privateParams);
+	
+	   if($keyword != null){
+	   	  $criteria = "Region.id like '$keyword%' or Region.region_name like '%$keyword%' ";
+	   }
+       
+       list($order,$limit,$page) = $this->Pagination->init($criteria,null,array('ajaxDivUpdate'=>'cs','url'=> 'index/'.$keyword));
+       
+       //list($order,$limit,$page) = $this->Pagination->init($criteria,null,$settings); // Added
+       $data = $this->Region->findAll($criteria, NULL, $order, $limit, $page);  		
 		
-		$this->Pagination->resultsPerPage = array(100,200,500);
-		$this->Pagination->show = 10;
-		$this->Pagination->style = "ajax";
-		$this->Session->write('search_term', $this->data['Region']['keyword']);
-
-		$this->Pagination->privateParams  = Array('Region.id' => '110000');
-
-		$settings = array (
-			'show' => 15,
-			'privateParams' => array('Region.id' => '110000'),
-		); 
-		
-		list($order,$limit,$page) = $this->Pagination->init($criteria,null,$settings); // Added
-
-		//list($order,$limit,$page) = $this->Pagination->init(null,array('dd'=>'Foo'),null);
-		$data = $this->Region->findAll($criteria, NULL, $order, $limit, $page); // Extra parameters added
-		
-		
+		//list($order,$limit,$page) = $this->Pagination->init($criteria,null,$settings); // Added
+		//$data = $this->Region->findAll($criteria, NULL, $order, $limit, $page); // Extra parameters added
+				
 		$this->set('regions', $data);
 	}
 
