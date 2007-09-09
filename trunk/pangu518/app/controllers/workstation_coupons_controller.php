df8dd541614b739f144caec30c6f1a5f73c777ac
longben <longben@4d87f418-fb31-0410-b12f-21406559f0cb>
2007-09-09 10:15:03 +0000

    临时提交

--- trunk/pangu518/app/controllers/workstation_coupons_controller.php
+++ trunk/pangu518/app/controllers/workstation_coupons_controller.php
@@ -9,7 +9,9 @@ class WorkstationCouponsController extends AppController {
 		$user_id = $this->Session->read('User.id');
 		$criteria = array(
 		  'Workstation.user_id' => $user_id
-		);		
+		);
+
+		
 		$this->data = $this->WorkstationCoupon->Workstation->find($criteria,null,'Workstation.id',null);
 		if(empty($this->data)){
 			$this->Session->setFlash('请先申请成立会员消费单位！');
@@ -25,12 +27,14 @@ class WorkstationCouponsController extends AppController {
 		  'Workstation.user_id' => $this->Session->read('User.id'),
 		  'WorkstationCoupon.status' => 131
 		);
+		$this->WorkstationCoupon->unbindModel(array('belongsTo' => array('Coupon')));
 		$this->set('total', $this->WorkstationCoupon->findCount($criteria));
 		
 		$criteria = array(
 		  'Workstation.user_id' => $this->Session->read('User.id'),
 		  'WorkstationCoupon.status' => 341
 		);
+		$this->WorkstationCoupon->unbindModel(array('belongsTo' => array('Coupon')));
 		$this->set('total_sale', $this->WorkstationCoupon->findCount($criteria));
 		
 		$this->WorkstationCoupon->unbindModel(array('belongsTo' => array('Coupon')));
