9bbbbd91f4a1279cbd74fcde9eea2bd20efbae42
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2008-03-09 18:29:18 +0000

    news

--- pangu518/app/models/article.php
+++ pangu518/app/models/article.php
@@ -42,17 +42,26 @@ class Article extends AppModel {
 				),
 
 	);
-
+	
+	//通过频道查询最新N条数据
 	function findArticleByChannel($channel_id = null,$limit = null){
 		$this->recursive = 0;
 		return $this->findAll("Article.channel_id = $channel_id order by Article.created desc limit $limit");
 	}
+	
+	//通过频道查询点击量最大·热门N条数据
+	function findArticleByChannelClick($channel_id = null,$limit = null){
+		$this->recursive = 0;
+		return $this->findAll("Article.channel_id = $channel_id order by Article.hits desc limit $limit");
+	}
 
+	//通过栏目查询最新N条数据
 	function findArticleByWebpage($webpage_id = null,$limit = null){
 		$this->recursive = 0;
 		return $this->findAll("Article.webpage_id = $webpage_id order by Article.created desc limit $limit");
 	}
 
+	//取出图片
 	function findMerchant($channel_id = null,$limit = null){
 		$this->recursive = 0;
 		return $this->findAll("Article.channel_id = $channel_id 
