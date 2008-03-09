7fc173e5387ea5861bf5caaecd53d2325d5ee029
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2008-03-09 15:40:09 +0000

    www框架页面

--- trunk/pangu518/app/controllers/homes_controller.php
+++ trunk/pangu518/app/controllers/homes_controller.php
@@ -53,5 +53,70 @@ class HomesController extends AppController {
 		$this->set('lotterites', $this->Lottery->findBulletin(4)); //开奖公告
 	}
 
+	function workstations_index() {
+		$this->layout = 'website';
+		$this->set('zxxxs', $this->Article->findArticleByChannel($this->ZXXX,9)); //最新消息
+		$this->set('cfzxs', $this->Article->findArticleByWebpage($this->CFZX,6)); //财富资讯;
+		$this->set('xfscs', $this->Article->findArticleByWebpage($this->XFSC,6)); //消费市场;
+		$this->set('mypgs', $this->Article->findArticleByWebpage($this->MYPG,7)); //我与消费财富网;
+		$this->set('sjxxs', $this->Article->findArticleByChannel($this->SJXX,7)); //商家信息;
+
+		$this->set('merchants', $this->Article->findMerchant($this->SJXX,7)); //首页商家滚动图片
+
+		$this->set('lotterites', $this->Lottery->findBulletin(4)); //开奖公告
+	}
+
+	function cs_center_index() {
+		$this->layout = 'website';
+		$this->set('zxxxs', $this->Article->findArticleByChannel($this->ZXXX,9)); //最新消息
+		$this->set('cfzxs', $this->Article->findArticleByWebpage($this->CFZX,6)); //财富资讯;
+		$this->set('xfscs', $this->Article->findArticleByWebpage($this->XFSC,6)); //消费市场;
+		$this->set('mypgs', $this->Article->findArticleByWebpage($this->MYPG,7)); //我与消费财富网;
+		$this->set('sjxxs', $this->Article->findArticleByChannel($this->SJXX,7)); //商家信息;
+
+		$this->set('merchants', $this->Article->findMerchant($this->SJXX,7)); //首页商家滚动图片
+
+		$this->set('lotterites', $this->Lottery->findBulletin(4)); //开奖公告
+	}
+
+	function legal_index() {
+		$this->layout = 'website';
+		$this->set('zxxxs', $this->Article->findArticleByChannel($this->ZXXX,9)); //最新消息
+		$this->set('cfzxs', $this->Article->findArticleByWebpage($this->CFZX,6)); //财富资讯;
+		$this->set('xfscs', $this->Article->findArticleByWebpage($this->XFSC,6)); //消费市场;
+		$this->set('mypgs', $this->Article->findArticleByWebpage($this->MYPG,7)); //我与消费财富网;
+		$this->set('sjxxs', $this->Article->findArticleByChannel($this->SJXX,7)); //商家信息;
+
+		$this->set('merchants', $this->Article->findMerchant($this->SJXX,7)); //首页商家滚动图片
+
+		$this->set('lotterites', $this->Lottery->findBulletin(4)); //开奖公告
+	}
+
+	function consumer_market_index() {
+		$this->layout = 'website';
+		$this->set('zxxxs', $this->Article->findArticleByChannel($this->ZXXX,9)); //最新消息
+		$this->set('cfzxs', $this->Article->findArticleByWebpage($this->CFZX,6)); //财富资讯;
+		$this->set('xfscs', $this->Article->findArticleByWebpage($this->XFSC,6)); //消费市场;
+		$this->set('mypgs', $this->Article->findArticleByWebpage($this->MYPG,7)); //我与消费财富网;
+		$this->set('sjxxs', $this->Article->findArticleByChannel($this->SJXX,7)); //商家信息;
+
+		$this->set('merchants', $this->Article->findMerchant($this->SJXX,7)); //首页商家滚动图片
+
+		$this->set('lotterites', $this->Lottery->findBulletin(4)); //开奖公告
+	}
+
+	function fortune_index() {
+		$this->layout = 'website';
+		$this->set('zxxxs', $this->Article->findArticleByChannel($this->ZXXX,9)); //最新消息
+		$this->set('cfzxs', $this->Article->findArticleByWebpage($this->CFZX,6)); //财富资讯;
+		$this->set('xfscs', $this->Article->findArticleByWebpage($this->XFSC,6)); //消费市场;
+		$this->set('mypgs', $this->Article->findArticleByWebpage($this->MYPG,7)); //我与消费财富网;
+		$this->set('sjxxs', $this->Article->findArticleByChannel($this->SJXX,7)); //商家信息;
+
+		$this->set('merchants', $this->Article->findMerchant($this->SJXX,7)); //首页商家滚动图片
+
+		$this->set('lotterites', $this->Lottery->findBulletin(4)); //开奖公告
+	}
+
 }
 ?>
