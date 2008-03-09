7bc146b53f457afbf9265910c5eff0d15a8468d6
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2008-03-09 16:50:25 +0000

    www

--- pangu518/app/controllers/homes_controller.php
+++ pangu518/app/controllers/homes_controller.php
@@ -118,5 +118,17 @@ class HomesController extends AppController {
 		$this->set('lotterites', $this->Lottery->findBulletin(4)); //开奖公告
 	}
 
+	function cooperation_index() {
+		$this->layout = 'website';
+		$this->set('zxxxs', $this->Article->findArticleByChannel($this->ZXXX,9)); //最新消息
+		$this->set('cfzxs', $this->Article->findArticleByWebpage($this->CFZX,6)); //财富资讯;
+		$this->set('xfscs', $this->Article->findArticleByWebpage($this->XFSC,6)); //消费市场;
+		$this->set('mypgs', $this->Article->findArticleByWebpage($this->MYPG,7)); //我与消费财富网;
+		$this->set('sjxxs', $this->Article->findArticleByChannel($this->SJXX,7)); //商家信息;
+
+		$this->set('merchants', $this->Article->findMerchant($this->SJXX,7)); //首页商家滚动图片
+
+		$this->set('lotterites', $this->Lottery->findBulletin(4)); //开奖公告
+	}
 }
 ?>
