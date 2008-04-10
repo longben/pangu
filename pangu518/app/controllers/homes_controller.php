1062637eb699053383a2506fbc1fabd010607f9d
cloudlin <cloudlin@4d87f418-fb31-0410-b12f-21406559f0cb>
2008-04-10 09:53:59 +0000

    首页 合作机会增加

--- pangu518/app/controllers/homes_controller.php
+++ pangu518/app/controllers/homes_controller.php
@@ -14,13 +14,14 @@ class HomesController extends AppController {
 	var $GZZFB = 37; //工作站分别
 
 	var $FHXX = 9; //分红信息
+	var $HZJH = 11; //合作机会
 
 	function index() {	//首页
 		$this->layout = 'website';
 		$this->set('zxxxs', $this->Article->findArticleByChannel($this->ZXXX,9)); //最新消息
 		$this->set('cfzxs', $this->Article->findArticleByWebpage($this->CFZX,6)); //财富资讯;
 		$this->set('xfscs', $this->Article->findArticleByWebpage($this->XFSC,6)); //消费市场;
-		$this->set('mypgs', $this->Article->findArticleByWebpage($this->MYPG,7)); //我与消费财富网;
+		$this->set('hzjhs', $this->Article->findArticleByWebpage($this->HZJH,11)); //合作机会;
 		$this->set('sjxxs', $this->Article->findArticleByChannel($this->SJXX,7)); //商家信息;
 
 		$this->set('merchants', $this->Article->findMerchant($this->SJXX,7)); //首页商家滚动图片
